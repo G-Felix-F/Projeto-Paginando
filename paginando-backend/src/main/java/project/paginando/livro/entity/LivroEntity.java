24a72a2c07e3c11c9b75f7a8ef106470608bac55
Gabriel <gfelixfaustina@gmail.com>
2025-03-24 19:58:10 -0300

    Atualizado as entidades Livro e Usuario

--- paginando-backend/src/main/java/project/paginando/livro/entity/LivroEntity.java
+++ paginando-backend/src/main/java/project/paginando/livro/entity/LivroEntity.java
@@ -12,6 +12,20 @@ import java.util.Set;
 @Table(name = "livros")
 public class LivroEntity {
 
+    // CONSTRUTORES
+
+    public LivroEntity(String titulo, String autor, String isbn, String descricao,
+                       LivroGeneroEnum genero, String editora) {
+        setTitulo(titulo);
+        setAutor(autor);
+        setIsbn(isbn);
+        setDescricao(descricao);
+        setGenero(genero);
+        setEditora(editora);
+    }
+
+    // ATRIBUTOS
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -40,5 +54,77 @@ public class LivroEntity {
     @ManyToMany(mappedBy = "donos")
     private Set<UsuarioEntity> donos = new HashSet<>();
 
+    // GETTERS E SETTERS
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LivroGeneroEnum getGenero() {
+        return genero;
+    }
+
+    public void setGenero(LivroGeneroEnum genero) {
+        this.genero = genero;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public Set<AvaliacaoEntity> getAvaliacoes() {
+        return avaliacoes;
+    }
+
+    public void setAvaliacoes(Set<AvaliacaoEntity> avaliacoes) {
+        this.avaliacoes = avaliacoes;
+    }
+
+    public Set<UsuarioEntity> getDonos() {
+        return donos;
+    }
+
+    public void setDonos(Set<UsuarioEntity> donos) {
+        this.donos = donos;
+    }
 }
--- paginando-backend/src/main/java/project/paginando/usuario/UsuarioEntity.java
+++ paginando-backend/src/main/java/project/paginando/usuario/UsuarioEntity.java
@@ -2,16 +2,29 @@ package project.paginando.usuario;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.Data;
 import project.paginando.livro.entity.LivroEntity;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
 @Entity
 @Table(name = "usuarios")
 public class UsuarioEntity {
+
+    // CONSTRUTORES
+
+    public UsuarioEntity(String username, String nome, String email, String senha,
+                         String fotoPerfil, String cidade, String estado) {
+        setUsername(username);
+        setNome(nome);
+        setEmail(email);
+        setSenha(senha);
+        setFotoPerfil(fotoPerfil);
+        setCidade(cidade);
+        setEstado(estado);
+    }
+
+    // ATRIBUTOS
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -49,4 +62,86 @@ public class UsuarioEntity {
             inverseJoinColumns = @JoinColumn(name = "livro_id")
     )
     private Set<LivroEntity> livros = new HashSet<>();
+
+    // GETTERS E SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getFotoPerfil() {
+        return fotoPerfil;
+    }
+
+    public void setFotoPerfil(String fotoPerfil) {
+        this.fotoPerfil = fotoPerfil;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Set<LivroEntity> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(Set<LivroEntity> livros) {
+        this.livros = livros;
+    }
 }
